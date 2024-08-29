6503459ef394bd0a486c54d1d9cb0979bf8ba443
Anton Prafanto <anton.prafanto@gmail.com>
2024-08-29 08:31:36 +0800

    Container, Box Decoration

--- pertemuan3_kelas_a/lib/main.dart
+++ pertemuan3_kelas_a/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pertemuan3_kelas_a/widgets/container.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,43 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Ini RichText'),
-        ),
-        body: RichText(
-          text: const TextSpan(
-            children: [
-              TextSpan(
-                text: 'Hallo',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 20,
-                      color: Colors.black45,
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-              ),
-              TextSpan(
-                text: ' Flutter',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextSpan(
-                text: ' !',
-                style: TextStyle(fontSize: 30, color: Colors.red),
-              ),
-            ],
-          ),
-        ),
+        body: MyContainer(),
       ),
     );
   }
---
+++ pertemuan3_kelas_a/lib/widgets/container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyContainer extends StatelessWidget {
+  const MyContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Belajar Container'),
+      ),
+      body: Container(
+        height: 300,
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [Colors.black12, Colors.blueAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          border: Border.all(
+            width: 3,
+            color: Colors.cyan,
+          ),
+        ),
+      ),
+    );
+  }
+}
